400da8c9a75e3db0e5759a9ac1c97e5e5ee2277a
Bunyod <bunyodreal@gmail.com>
2020-04-05 22:09:47 +0200

    add routes unit tests

---
+++ src/test/scala/bunyod/fp/http/items/ItemRoutesSpec.scala
@@ -0,0 +1,64 @@
+package bunyod.fp.http.items
+
+import bunyod.fp.domain.brands.BrandsPayloads.BrandName
+import bunyod.fp.domain.items.ItemsPayloads._
+import bunyod.fp.domain.items._
+import bunyod.fp.http.utils.json._
+import bunyod.fp.suite.Arbitraries._
+import bunyod.fp.suite.HttpTestSuite
+import cats.effect._
+import cats.implicits._
+import org.http4s.Method._
+import org.http4s._
+import org.http4s.client.dsl.io._
+
+class ItemRoutesSpec extends HttpTestSuite {
+
+  def dataItems(items: List[Item]) = new ItemsService[IO](
+    new TestItems {
+
+      override def findAll: IO[List[Item]] =
+        IO.pure(items)
+    }
+  )
+
+  def failingItems(items: List[Item]) = new ItemsService[IO](
+    new TestItems {
+
+      override def findAll: IO[List[Item]] =
+        IO.raiseError(DummyError) *> IO.pure(items)
+
+      override def findBy(brand: BrandName): IO[List[Item]] =
+        findAll
+    }
+  )
+
+  forAll { i: List[Item] =>
+    spec("GET items [OK]") {
+      GET(Uri.unsafeFromString("/items")).flatMap { req =>
+        val routes = new ItemRoutes[IO](dataItems(i)).routes
+        assertHttp(routes, req)(Status.Ok, i)
+      }
+    }
+  }
+
+  forAll { i: List[Item] =>
+    spec("GET items [ERROR]") {
+      GET(Uri.unsafeFromString("/items")).flatMap { req =>
+        val routes = new ItemRoutes[IO](failingItems(i)).routes
+        assertHttpFailure(routes, req)
+      }
+    }
+  }
+
+}
+
+protected class TestItems extends ItemsAlgebra[IO] {
+
+  override def findAll: IO[List[Item]] = IO.pure(List.empty)
+  override def findBy(brand: BrandName): IO[List[Item]] = IO.pure(List.empty)
+  override def findById(itemId: ItemId): IO[Option[Item]] = IO.pure(none[Item])
+  override def create(item: ItemsPayloads.CreateItem): IO[Unit] = IO.unit
+  override def update(item: ItemsPayloads.UpdateItem): IO[Unit] = IO.unit
+
+}
