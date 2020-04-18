df9b3b3cbef0c10c4694dda33e133a84a9a65ea6
Scala Steward <me@scala-steward.org>
2020-04-18 02:10:51 +0200

    Update context-applied to 0.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val refined = "0.9.13"
 
     val betterMonadicFor = "0.3.1"
-    val contextApplied = "0.1.3"
+    val contextApplied = "0.1.4"
     val kindProjector = "0.11.0"
     val skunk = "0.0.8"
     val http4s = "0.21.3"
