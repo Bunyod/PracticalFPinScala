732f5c7dc05c8a6149e20bf56cccb52fcbf86114
Scala Steward <me@scala-steward.org>
2020-11-11 20:16:03 +0100

    Update http4s-blaze-client, ... to 0.21.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val contextApplied = "0.1.4"
     val kindProjector = "0.11.0"
     val skunk = "0.0.17"
-    val http4s = "0.21.3"
+    val http4s = "0.21.9"
     val circe = "0.13.0"
     val http4sJwtAuth = "0.0.4"
     val log4cats = "1.0.1"
