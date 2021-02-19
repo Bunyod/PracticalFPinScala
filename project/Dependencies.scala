02273642c9bcdf28f858df1a2602bde42839d3fa
Scala Steward <me@scala-steward.org>
2021-02-19 16:14:51 +0100

    Update scalatest to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     val pureConfig = "0.14.0"
 
     val scalaCheck    = "1.15.2"
-    val scalaTest     = "3.2.2"
+    val scalaTest     = "3.2.5"
     val scalaTestPlus = "3.2.0.0"
   }
 
