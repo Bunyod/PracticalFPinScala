50f3e842e3bb7ca05cbdda8359445ee7d6c62b93
Scala Steward <me@scala-steward.org>
2021-04-01 14:26:33 +0200

    Update redis4cats-effects, ... to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val http4sJwtAuth = "0.0.4"
     val log4cats = "1.1.1"
     val catsRetry = "2.0.0"
-    val redis4cats = "0.10.3"
+    val redis4cats = "0.13.1"
     val ciris = "1.2.1"
     val pureConfig = "0.14.1"
 
