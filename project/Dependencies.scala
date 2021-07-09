7ba99d08ef4a698246656f07b2cfd93c7e0196f4
Scala Steward <me@scala-steward.org>
2021-07-09 10:17:35 +0200

    Update cats-retry to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val circe = "0.14.1"
     val http4sJwtAuth = "0.0.6"
     val log4cats = "1.1.1"
-    val catsRetry = "2.1.1"
+    val catsRetry = "3.0.0"
     val redis4cats = "0.14.0"
     val ciris = "2.0.1"
     val pureConfig = "0.16.0"
