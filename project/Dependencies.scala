809026696438cab73abac1dc4f6147e566253441
Scala Steward <me@scala-steward.org>
2021-08-29 00:35:44 +0200

    Update cats-effect to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val cats = "2.6.1"
-    val catsEffect = "3.2.0"
+    val catsEffect = "3.2.5"
     val catsMeowMtl = "0.5.0"
     val fs2 = "3.0.6"
     val logback = "1.2.5"
