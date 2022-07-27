b219bbe346538a7cc803e3afab2f89ec207a7ef1
Scala Steward <scala_steward@virtuslab.com>
2022-07-27 12:53:31 +0000

    Update fs2-core to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val cats = "2.7.0"
     val catsEffect = "3.3.0"
-    val fs2 = "3.1.6"
+    val fs2 = "3.2.11"
     val logback = "1.2.7"
     val newtype = "0.4.3"
     val refined = "0.9.28"
