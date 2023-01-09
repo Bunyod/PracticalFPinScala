701ce672cd60d6a60c3ccdd3695a6bcf7d5a7b1b
Scala Steward <scala_steward@virtuslab.com>
2023-01-09 15:28:22 +0000

    Update scalatest to 3.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val pureConfig = "0.17.1"
 
     val scalaCheck = "1.15.4"
-    val scalaTest = "3.2.11"
+    val scalaTest = "3.2.15"
     val scalaTestPlus = "3.2.2.0"
   }
 
